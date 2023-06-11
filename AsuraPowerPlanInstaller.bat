a065748409d7c3c996641f9bb821bf840b30edad
AsuraTheCoder <136213464+AsuraTheCoder@users.noreply.github.com>
2023-06-11 08:34:12 +0530

    Add files via upload

---
+++ AsuraPowerPlanInstaller.bat
@@ -0,0 +1,17 @@
+@echo off
+
+echo Adding Power Plans
+
+powercfg -import %~dp0LegionBalance.pow 1d949fed-9593-464f-9158-58ca3b8b38c7
+
+powercfg -import %~dp0LegionQuiet.pow 9f47f895-74d8-4146-b6f4-ad03b5737ed1
+
+powercfg -import %~dp0LegionPerformance.pow b4fbffc4-358f-4314-8049-24db4a306cb0
+
+powercfg -SETACTIVE 1d949fed-9593-464f-9158-58ca3b8b38c7
+
+powercfg delete 381b4222-f694-41f0-9685-ff5bb260df2e
+
+pause
+
+Property of @AsuraTheCoder
