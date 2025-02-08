baa9bcc49cd32d3aac48f5dd4a387a41bb42af41
AsuraTheCoder <136213464+AsuraTheCoder@users.noreply.github.com>
2025-02-08 22:39:26 +0530

    Add files via upload
    Updated for more refined tweaking and per user performance. Use "PowerPlanOptions.reg" to enable the hidden power plan options on your pc and tweak them according to your system

--- AsuraPowerPlanInstaller.bat
+++ AsuraPowerPlanInstaller.bat
@@ -6,7 +6,9 @@ powercfg -import %~dp0LegionBalance.pow 1d949fed-9593-464f-9158-58ca3b8b38c7
 
 powercfg -import %~dp0LegionQuiet.pow 9f47f895-74d8-4146-b6f4-ad03b5737ed1
 
-powercfg -import %~dp0LegionPerformance.pow b4fbffc4-358f-4314-8049-24db4a306cb0
+powercfg -import %~dp0LegionPerformance.pow 80bba9a0-4a73-447c-8a25-f71c8586ad49
+
+powercfg -import %~dp0LegionUltimatePerformance.pow b4fbffc4-358f-4314-8049-24db4a306cb0
 
 powercfg -SETACTIVE 1d949fed-9593-464f-9158-58ca3b8b38c7
 
